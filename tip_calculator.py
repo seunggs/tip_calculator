14df4994e9d3360472edc6e07b730d576c84fb7d
seunggs <seunggs@gmail.com>
2014-04-17 00:19:27 -0400

    Minor improvements

--- tip_calculator.py
+++ tip_calculator.py
@@ -7,7 +7,7 @@ meal_with_tax = meal * (1 + tax)
 tip_value = meal_with_tax * tip
 total = meal_with_tax + tip_value
 
-print "The base cost of your meal was ${:.2f}".format(meal)
-print "You need to pay ${:.2f} for tax.".format(tax_value)
-print "Tipping at a rate of {:.0f}%, you should leave ${:.2f} for a tip".format(tip * 100, tip_value)
-print "The grand total of your meal is ${:.2f}.".format(total)
+print "The base cost of your meal was ${0:.2f}".format(meal)
+print "You need to pay ${0:.2f} for tax.".format(tax_value)
+print "Tipping at a rate of {0}%, you should leave ${1:.2f} for a tip".format(int(tip * 100), tip_value)
+print "The grand total of your meal is ${0:.2f}.".format(total)
